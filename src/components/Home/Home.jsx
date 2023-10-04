e59b9239fddbea12cfef6f360bb0332785328a57
lonewolf1503 <harshmathur1503@gmail.com>
2023-10-04 18:38:40 +0530

    heading changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
   return (
     <div>
       <Mainpage />
-      <Threads />
-      <Footer />
+      {/* <Threads />
+      <Footer /> */}
     </div>
   );
 }
--- src/components/Home/childs/Mainpage.jsx
+++ src/components/Home/childs/Mainpage.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const Mainpage = () => {
   return (
-    <div>
-        <h1>
-            Mainpage
-        </h1>
+    <div className="flex flex-col items-center h-screen">
+      <div className="relative mt-12">
+        <img src="/images/logo2.svg" alt="Your Image" className="mx-auto" />
+      </div>
+      <div>
+        <img
+          src="/images/red.svg"
+          alt="red-image"
+          className="absolute top-0 left-0"
+        />
+        <img
+          src="/images/blue.svg"
+          alt="blue-image"
+          className="absolute top-0 right-0"
+        />
+      </div>
+      <div className="text-5xl font-[700] text-center justify-center mt-6">
+        <h1>Google Developer Student Club</h1>
+      </div>
+      <div className="text-3xl font-[#141718] mt-2">
+        <h2>Jaypee Institute Of Information Technology 62</h2>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Mainpage
+export default Mainpage;
