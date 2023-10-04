057b849f13fb594bd46661e027e35992afd54734
lonewolf1503 <harshmathur1503@gmail.com>
2023-10-04 23:25:19 +0530

    reload issue solved

--- src/components/Home/childs/Animation.jsx
+++ src/components/Home/childs/Animation.jsx
@@ -1,72 +1,75 @@
-import React, { useState }  from 'react'
-import './Animation.css'
+import React, { useState, useEffect } from 'react';
+import './Animation.css';
 
 const Animation = () => {
+  const [activeButton, setActiveButton] = useState(
+    localStorage.getItem('activeButton') || 'home-clicked'
+  );
 
-    const [activeButton, setActiveButton] = useState('home-clicked');
+  const handleButtonClick = (className) => {
+    setActiveButton(className);
+    localStorage.setItem('activeButton', className);
+  };
 
-    const handleButtonClick = (className) => {
-        setActiveButton(className);
-    };
+  useEffect(() => {
+    const storedActiveButton = localStorage.getItem('activeButton');
+    if (storedActiveButton) {
+      setActiveButton(storedActiveButton);
+    }
+  }, []);
 
-    return (
-        <div className='relative'>
+  return (
+    <div className="relative">
+      <div className="absolute">
+        <button
+          className={`categories home-head ${
+            activeButton === 'home-clicked' ? 'clicked' : ''
+          }`}
+          onClick={() => handleButtonClick('home-clicked')}
+        >
+          Home
+        </button>
 
-            <div className='absolute'>
+        <button
+          className={`categories events-head ${
+            activeButton === 'events-clicked' ? 'clicked' : ''
+          }`}
+          onClick={() => handleButtonClick('events-clicked')}
+        >
+          Events
+        </button>
 
-                <button className={`categories home-head ${
-                        activeButton === 'home-clicked' ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('home-clicked')}
-                >
-                    Home
-                </button>
+        <button
+          className={`categories team-head ${
+            activeButton === 'team-clicked' ? 'clicked' : ''
+          }`}
+          onClick={() => handleButtonClick('team-clicked')}
+        >
+          Our Team
+        </button>
 
-                <button className={`categories events-head ${
-                        activeButton === 'events-clicked' ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('events-clicked')}
-                >
-                    Events
-                </button>
+        <button
+          className={`categories what-head ${
+            activeButton === 'what-clicked' ? 'clicked' : ''
+          }`}
+          onClick={() => handleButtonClick('what-clicked')}
+        >
+          What we do
+        </button>
 
-                <button className={`categories team-head ${
-                        activeButton === 'team-clicked' ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('team-clicked')}
-                >
-                    Our Team
-                </button>
+        <div className={`outer-circle-pointer ${activeButton || ''}`}></div>
 
-                <button className={`categories what-head ${
-                        activeButton === 'what-clicked' ? 'clicked' : ''
-                    }`}
-                    onClick={() => handleButtonClick('what-clicked')}
-                >
-                    What we do
-                </button>
-
-
-                
-                <div className={`outer-circle-pointer ${activeButton || ''}`}></div>
-                
-                <div class="path">                    
-                    <div class="outer-circle"></div>
-                    <div class="outer-circle2"></div> 
-                </div>
-
-
-                    
-                <div className={`semi circle ${activeButton || ''}`}>
-                    <div class={`semi2 ${activeButton || ''}`}></div>
-                </div>
-
-            </div>
-
-            
-            
+        <div class="path">
+          <div class="outer-circle"></div>
+          <div class="outer-circle2"></div>
         </div>
-    )
-}
 
-export default Animation
+        <div className={`semi circle ${activeButton || ''}`}>
+          <div className={`semi2 ${activeButton || ''}`}></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Animation;
