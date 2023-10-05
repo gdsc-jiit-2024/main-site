fd7a775a90235d2053ed946c15cfcd3cc44f1218
lonewolf1503 <harshmathur1503@gmail.com>
2023-10-05 23:35:05 +0530

    mainage updated

--- src/components/Home/childs/Mainpage.jsx
+++ src/components/Home/childs/Mainpage.jsx
@@ -23,14 +23,31 @@ const Mainpage = () => {
       <div className="text-5xl font-[700] text-center justify-center mt-6">
         <h1>Google Developer Student Club</h1>
       </div>
-      <div className="text-3xl font-[#141718] mt-2">
+      <div className="text-3xl text-[#6F6F6F] mt-4">
         <h2>Jaypee Institute Of Information Technology 62</h2>
       </div>
-
+      <div className="text-4xl font-[600] text-center justify-center mt-14">
+        <h1>An Electrifying Hub Of Innovation, Created By Students, Fueled</h1>
+      </div>
+      <div className="text-4xl font-[600] text-center justify-center mt-3">
+        <h1>By Students</h1>
+      </div>
+      <div className="text-4xl font-[600] text-center justify-center mt-3">
+        <h1>A Dynamic Developers Community!</h1>
+      </div>
+      <div className="text-2xl text-[#6F6F6F] mt-12 text-center mx-auto max-w-6xl">
+        <h2>
+          At GDSC JIIT 62, we aspire to ignite the flames of innovation within
+          young developers, guiding them to underneath their hidden talents and
+          embark on a transformative journey of{' '}
+          <span className="text-black inline-block p-1 border-4 border-blue-500 bg-blue-300 rounded-3xl px-3.5">
+            self-discovery
+          </span>
+        </h2>
+      </div>
       <div className='pt-20 me-auto'>
         <Animation />
       </div>
-
     </div>
   );
 };
